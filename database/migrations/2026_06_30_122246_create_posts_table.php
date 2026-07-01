6f1846963b13d0d888f0e1757e5ac7dcfaeef258
Ale2xK <kino122122@gmail.com>
2026-07-01 19:16:14 +0300

    used another approach for migration

--- database/migrations/2026_06_30_122246_create_posts_table.php
+++ database/migrations/2026_06_30_122246_create_posts_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
             $table->timestamps();
-            $table->string('title');
-            $table->text('body');
         });
     }
 
---
+++ database/migrations/2026_07_01_160715_added_columns_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title');
+            $table->text('body');
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('title');
+            $table->dropColumn('body');
+            $table->dropColumn('user_id');
+            $table->dropForeign('posts_user_id_foreign');
+        });
+    }
+};
